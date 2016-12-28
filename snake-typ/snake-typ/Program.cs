57188bc56c1ab9aac4478e4ab1f88d56f255bb5f
NatSoboleva <nsoboleva734@gmail.com>
2016-12-28 15:44:01 +0300

    добавлены горизонтальные и вертикальные линии

--- snake-typ/snake-typ/Program.cs
+++ snake-typ/snake-typ/Program.cs
@@ -10,55 +10,19 @@ namespace snake_typ
     {
         static void Main(string[] args)
         {
-           /* Point p1 = new Point(1,3,'*');
+            Point p1 = new Point(1,3,'*');
             p1.Draw();
 
             Point p2 = new Point(4,5, '#');
-            p2.Draw();*/
+            p2.Draw();
 
-            List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
+            HorizontalLine line = new HorizontalLine(5,10,8, '+');
+            line.Drow();
 
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
-
-            foreach(int i in numList)
-            {
-                Console.WriteLine(i);
-            }
-
-            numList.RemoveAt(0);
-
-            Point p1 = new Point(1, 7, '*');
-            Point p2 = new Point(4, 9, '#');
-            Point p3 = new Point(8, 10, '%');
-
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
-            pList.Add(p3);
-
-            foreach(Point p in pList)
-            {
-                p.Draw();
-            }
-
-            List<char> charList = new List<char>();
-            charList.Add('@');
-            charList.Add('&');
-            charList.Add('$');
-            charList.Add('#');
-
-            foreach(char c in charList)
-            {
-                Console.WriteLine(c);
-            }
+            VerticalLine lines = new VerticalLine(9, 15, 6, '*');
+            lines.Drow();
 
 
-           
             Console.ReadLine();
         }
 
