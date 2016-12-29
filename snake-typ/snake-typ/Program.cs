d221f6c77c59d2bebcc9820994ea2dd124ddcdc5
NatSoboleva <nsoboleva734@gmail.com>
2016-12-29 17:45:38 +0300

    управляемая змейка

--- snake-typ/snake-typ/Program.cs
+++ snake-typ/snake-typ/Program.cs
@@ -12,14 +12,14 @@ namespace snake_typ
         static void Main(string[] args)
         {
             Console.SetBufferSize(80, 25);
-            
+
             //Отрисовка рамочки
-            HorizontalLine upLine = new HorizontalLine(0,78,0, '+');
+            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
             HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
             VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
             VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
             upLine.Drow();
-            downLine.Drow();            
+            downLine.Drow();
             leftLine.Drow();
             rightLine.Drow();
 
@@ -27,25 +27,17 @@ namespace snake_typ
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Drow();
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
+
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.HandleKey(key.Key);
+                }
+                Thread.Sleep(100);
+                snake.Move();
+            }
         }
 
     }
